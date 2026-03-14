d674182f829b5e799cfe784a8b33ef59a5cfae42
Freddie Taylor <freddiejljtaylor@gmail.com>
2026-03-14 19:08:47 +0000

    Adds internal components

---
+++ src/engine_interfaces/objects/components/CameraComponent.java
@@ -0,0 +1,10 @@
+﻿package engine_interfaces.objects.components;
+
+import engine_interfaces.objects.Component;
+import engine_interfaces.objects.Point;
+
+public class CameraComponent extends Component {
+    public Point Origin;
+    public int viewWidth;
+    public int viewHeight;
+}
---
+++ src/engine_interfaces/objects/components/RenderableComponent.java
@@ -0,0 +1,9 @@
+﻿package engine_interfaces.objects.components;
+
+import engine_interfaces.objects.Component;
+
+public class RenderableComponent extends Component {
+    public char Char;
+    public String fgColour;
+    public String bgColour;
+}

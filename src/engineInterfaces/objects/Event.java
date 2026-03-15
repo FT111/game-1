41429df0f8044656c08dd73afcb5965ffbf0a88f
Freddie Taylor <freddiejljtaylor@gmail.com>
2026-03-15 09:08:01 +0000

    Adds game event abstract class

---
+++ src/engineInterfaces/objects/Event.java
@@ -0,0 +1,21 @@
+﻿package engineInterfaces.objects;
+
+/// Event bus event
+public abstract class Event {
+    private final long tick;
+
+    protected Event (long tick) {
+        assert tick >= 0 : "Event tick must be non-negative";
+        this.tick = tick;
+    }
+
+    public long getTick() {
+        return tick;
+    }
+
+    // For serialization
+    @Override
+    public String toString() {
+        return "event-"+getClass().getSimpleName()+"-t" + tick;
+    }
+}

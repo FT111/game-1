18dd81327d4527d8495cfd1dc57dfcc3dab1e457
FT111 <freddiejljtaylor@gmail.com>
2026-03-17 17:37:12 +0000

    Starts Lanterna wrapper implementation

--- src/engine/LanternaAPI.java
+++ src/engine/LanternaAPI.java
@@ -1,12 +1,35 @@
 ﻿package engine;
 
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.swing.SwingTerminal;
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 import engine_interfaces.objects.rendering.GraphicsAPI;
 import engine_interfaces.objects.rendering.RenderBuffer;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
 
 public class LanternaAPI implements GraphicsAPI {
+    SwingTerminalFrame terminal;
+    Screen screen;
+
+    public LanternaAPI() throws IOException {
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+        terminal = factory.createSwingTerminal();
+        screen = new TerminalScreen(terminal);
+
+        terminal.createBufferStrategy(1);
+        terminal.setVisible(true);
+    }
+
     @Override
     public void render(RenderBuffer buffer) {
+        for (int i = 0; i < buffer.cells.length; i++) {
+                for (int j = 0; j < buffer.cells[i].length; j++) {
 
+                }
+        }
     }
 
     @Override

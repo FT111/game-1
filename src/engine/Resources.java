224729b0414876fbd2fa3fdaaf15bd2d3881e03c
FT111 <freddiejljtaylor@gmail.com>
2026-03-19 12:54:05 +0000

    Adds basic resource loader methods

--- src/engine/Resources.java
+++ src/engine/Resources.java
@@ -1,5 +1,24 @@
 ﻿package engine;
 
+import engine_interfaces.objects.ResourceLoader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Resources {
+    protected final List<ResourceLoader> resourceLoaders = new ArrayList<>();
+    protected final HashMap<String, Object> loadedResources = new HashMap<>();
+
+    public void addResourceLoader(String key, ResourceLoader loader) {
+        resourceLoaders.add(loader);
+        loadedResources.put(key, loader.Load());
+    }
+
+        public Object getResource(String key) {
+            return loadedResources.get(key);
+        }
+
+        public void saveResource
 
 }
--- src/engine_interfaces/objects/ResourceLoader.java
+++ src/engine_interfaces/objects/ResourceLoader.java
@@ -1,4 +1,10 @@
 ﻿package engine_interfaces.objects;
 
-public interface ResourceLoader{
+import java.util.Dictionary;
+import java.util.HashMap;
+
+public interface ResourceLoader {
+    Object Load(HashMap<String, Object> params);
+    void Save();
+    boolean Saveable();
 }

83417c77730e5d6c1bcae7092de61e386868dd49
Freddie Taylor <freddiejljtaylor@gmail.com>
2026-03-19 16:53:37 +0000

    Adds World CRUD

--- src/engine/World.java
+++ src/engine/World.java
@@ -1,50 +1,63 @@
-﻿package engine;
+package engine;
 
 import engine_interfaces.objects.Component;
+import engine_interfaces.objects.ComponentIndex;
 import engine_interfaces.objects.EntityID;
 import engine_interfaces.objects.LayerID;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.UUID;
 
 /// Contains all entities and layers in the game world.
 /// Responsible for world data, but not logic.
 public class World {
-    public HashMap<EntityID, HashMap<Class<? extends Component>, ? extends Component>> Entities = new HashMap<>();
-    public HashMap<LayerID, HashMap<Class<? extends Component>, ? extends Component>> Layers = new HashMap<>();
+    public HashMap<EntityID, HashMap<Class<? extends Component>, Component>> Entities = new HashMap<>();
+    public HashMap<LayerID, HashMap<Class<? extends Component>, Component>> Layers = new HashMap<>();
 
-    public HashMap<Class<Component>, List<EntityID>> ComponentEntitiesIndex = new HashMap<>();
-    public HashMap<Class<Component>, List<LayerID>> ComponentLayersIndex = new HashMap<>();
-
-    public HashSet<EntityID> queryEntities(Class<? extends Component>[] requiredComponents) {
-        if (requiredComponents.length == 0) {
-            return new HashSet<>(Entities.keySet());
-        }
-        HashSet<EntityID> collectedEntities = new HashSet<>();
-
-        for (Class<? extends Component> componentClass : requiredComponents) {
-            if (!ComponentEntitiesIndex.containsKey(componentClass)) {
-                return new HashSet<>();
-            }
-
-            List<EntityID> entitiesWithComponent = ComponentEntitiesIndex.get(componentClass);
-            if (entitiesWithComponent.isEmpty()) {
-                return new HashSet<>();
-            }
-
-            if (collectedEntities.isEmpty()) {
-                collectedEntities.addAll(entitiesWithComponent);
-            } else {
-                collectedEntities.retainAll(entitiesWithComponent);
-            }
-        }
-
-        return collectedEntities;
+    public EntityID createEntity(String Id) {
+        EntityID newId = new EntityID(Id);
+        Entities.put(newId, new HashMap<>());
+        return newId;
     }
 
-    public HashSet<EntityID> queryEntities(Class<? extends Component> requiredComponent) {
-        return new HashSet<>(ComponentEntitiesIndex.getOrDefault(requiredComponent, List.of()));
+    public EntityID createEntity() {
+        EntityID newId = new EntityID(UUID.randomUUID().toString());
+        Entities.put(newId, new HashMap<>());
+        return newId;
     }
+
+    public LayerID createLayer(String Id) {
+        LayerID newId = new LayerID(Id);
+        Layers.put(newId, new HashMap<>());
+        return newId;
+    }
+
+    public LayerID createLayer() {
+        LayerID newId = new LayerID(UUID.randomUUID().toString());
+        Layers.put(newId, new HashMap<>());
+        return newId;
+    }
+
+    public void addComponentToEntity(EntityID entityId, Component component) {
+        HashMap<Class<? extends Component>, Component> components = Entities.get(entityId);
+        if (components == null) {
+            throw new IllegalArgumentException("Entity ID does not exist: " + entityId);
+        }
+        components.put(component.getClass(), component);
+        ComponentEntitiesIndex.add(entityId, component.getClass());
+    }
+
+    public void addComponentToLayer(LayerID layerId, Component component) {
+        HashMap<Class<? extends Component>,  Component> components = Layers.get(layerId);
+        if (components == null) {
+            throw new IllegalArgumentException("Layer ID does not exist: " + layerId);
+        }
+        components.put(component.getClass(), component);
+        ComponentLayersIndex.add(layerId, component.getClass());
+    }
+
+    public ComponentIndex<EntityID> ComponentEntitiesIndex = new ComponentIndex<>();
+    public ComponentIndex<LayerID> ComponentLayersIndex = new ComponentIndex<>();
 }

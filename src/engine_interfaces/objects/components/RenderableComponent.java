373db341308d5ab651afc1e8a59111aead17e35d
Freddie Taylor <freddiejljtaylor@gmail.com>
2026-03-20 20:16:12 +0000

    Minor class cleanup

--- src/engine_interfaces/objects/components/RenderableComponent.java
+++ src/engine_interfaces/objects/components/RenderableComponent.java
@@ -1,9 +1,19 @@
 package engine_interfaces.objects.components;
 
 import engine_interfaces.objects.Component;
+import engine_interfaces.objects.rendering.Colour;
 
 public class RenderableComponent extends Component {
     public char Char;
-    public String fgColour;
-    public String bgColour;
+    public Colour fgColour;
+    public Colour bgColour;
+
+    public boolean isVisible;
+
+    public RenderableComponent(char Char, Colour fgColour, Colour bgColour, boolean isVisible) {
+        this.Char = Char;
+        this.fgColour = fgColour;
+        this.bgColour = bgColour;
+        this.isVisible = isVisible;
+    }
 }
